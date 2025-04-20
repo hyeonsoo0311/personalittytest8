296da90f1176a6d59fd7f10a8fa20b974e1a86d6
hyeonsoo0311 <hyeeonsoo@gmail.com>
2025-04-20 10:03:41 +0900

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,5 @@ import React from 'react'
 import PersonalityTest from './PersonalityTest'
 
 export default function App() {
-  return (
-    <div className="min-h-screen bg-white text-gray-800">
-      <PersonalityTest />
-    </div>
-  )
+  return <PersonalityTest />
 }
